4651f9976257a724c7b0f8dfa2fc491b8b3691ff
cleancodetdd2 <cleancodetdd2@gmail.com>
2025-01-14 17:40:01 +0900

    Add files via upload

---
+++ hello/Printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main_t3(void) 
+{
+	printf("My\tfirst C\nprogram\n");
+	printf("My ");
+	printf("second");
+	printf(" string.");
+	return 0;
+}
---
+++ hello/hello_format.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main_t2()
+{
+	printf("%s\n", "Hello, C!");
+	return 0;
+}
