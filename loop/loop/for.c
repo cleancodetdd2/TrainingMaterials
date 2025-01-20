2b860224a03def7810bfda72cd1e4b0c25e0abf1
cleancodetdd2 <cleancodetdd2@gmail.com>
2025-01-20 17:40:49 +0900

    Add files via upload

---
+++ loop/loop/for.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+		 // 반복에 사용할 변수 i를 선언하고 0으로 초기화 
+	for (int i = 0; i < 100; i++)	// 한번 반복할때 마다 i를 1씩 증가시킴 
+	{				// i가 100보다 작을 때까지만 반복 
+		printf("Hello World!\n");
+	}
+	return 0;
+}
